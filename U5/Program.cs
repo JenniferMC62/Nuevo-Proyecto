419479f37f0c791d7e95941ee5aa81a81772d3df
Jennifer Macedo <126447294+JenniferMC62@users.noreply.github.com>
2023-05-02 19:37:02 +0000

    actualizacion

--- U5/Program.cs
+++ U5/Program.cs
@@ -7,9 +7,19 @@
         string? edadCapturada = Console.ReadLine();
         if(edadCapturada is not null)
         {
+            try{
             edad = int.Parse(edadCapturada);
+            }
+
+            catch(System.FormatException)
+            {
+                Console.WriteLine("El formato es incorrecto");
+            }
         }
+        if(edad is not null)
         Console.WriteLine($"Tu edad es {edad}");
+        else
+        Console.WriteLine($"No se capturó tu edad");
 
     }
 
